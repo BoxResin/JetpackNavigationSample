3d6b6b161301c15b826f1df6c62896519eb926d3
Minsuk Eom <boxresin@gmail.com>
2019-10-27 15:00:00 +0900

    첫 번째 커밋

---
+++ app/src/main/java/boxresin/test/jetpacknavigationsample/MainActivity.kt
@@ -0,0 +1,12 @@
+package boxresin.test.jetpacknavigationsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/** 메인 액티비티 */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
