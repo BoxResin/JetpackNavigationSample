53884cfb7b9eec5df298273fc7ae33e88b3fe17e
Minsuk Eom <boxresin@gmail.com>
2019-11-09 15:00:00 +0900

    메인 프래그먼트 레이아웃 완성

--- app/src/main/java/boxresin/test/jetpacknavigationsample/MainFragment.kt
+++ app/src/main/java/boxresin/test/jetpacknavigationsample/MainFragment.kt
@@ -1,6 +1,18 @@
 package boxresin.test.jetpacknavigationsample
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.view.menu.MenuBuilder
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 /** 메인 프래그먼트 */
-class MainFragment : Fragment(R.layout.fragment_main)
+class MainFragment : Fragment(R.layout.fragment_main) {
+    @SuppressLint("RestrictedApi")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // 툴바 메뉴 초기화
+        view.toolbar.inflateMenu(R.menu.fragment_main)
+        (view.toolbar.menu as? MenuBuilder)?.setOptionalIconsVisible(true)
+    }
+}
